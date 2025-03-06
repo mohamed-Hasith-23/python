a22b76e63acfe76db9996bba5430852a15a15e20
mohamed-Hasith-23 <hasith9943@gmail.com>
2025-03-07 00:49:25 +0530

    String index file added

--- String_index.py
+++ String_index.py
@@ -1,13 +1,27 @@
 Credit_card = input("Enter your credit card number like xxxx-xxxx-xxxx-xxxx: ")
 
-# TO find last 4 digit of credit card
+# # TO find last 4 digit of credit card
 
 last_four = Credit_card[-4:]
 
 print(last_four)
 
-# to reverse all numbers
+# # to reverse all numbers
 
 reverse = Credit_card[::-1]
 
 print(reverse)
+
+# to get Email And Seperate with Domain and Username
+
+Email = input("enter your emial adddress: ")
+
+index = Email.index("@")
+
+username = Email[:index]
+
+domain = Email[index + 1:]
+
+print(f"The Username of Email is {username}")
+
+print(f"the doamin of Email is {domain}")
--- string_methods_exe.py
+++ string_methods_exe.py
@@ -1,3 +1,7 @@
+# Name should be less than 12 Characters
+# Name doesn't have space
+# Name shouldn't  have Numbeers
+
 Name = input("Enter your name: ")
 
 if len(Name) < 12 and Name.count(" ") == 0 and Name.isalpha() == True:
