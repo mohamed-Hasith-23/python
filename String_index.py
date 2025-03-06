411723ec9d3807be80bdb21c4893c2d9137878a8
mohamed-Hasith-23 <hasith9943@gmail.com>
2025-03-07 00:38:29 +0530

    String Index Added

---
+++ String_index.py
@@ -0,0 +1,13 @@
+Credit_card = input("Enter your credit card number like xxxx-xxxx-xxxx-xxxx: ")
+
+# TO find last 4 digit of credit card
+
+last_four = Credit_card[-4:]
+
+print(last_four)
+
+# to reverse all numbers
+
+reverse = Credit_card[::-1]
+
+print(reverse)
